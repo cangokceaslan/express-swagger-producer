e04c569656b00731f92fab945f1eb4257f94bd70
Can Gökçeaslan <48398625+cangokceaslan@users.noreply.github.com>
2022-03-16 23:39:33 +0300

    d.ts Update

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 import { Express } from 'express';
-declare module 'express-swagger' {
+declare module 'express-swagger-producer' {
     function SwaggerGenerator(options: any): any;
     export default function ExpressSwagger(app: Express): typeof SwaggerGenerator;
 }
